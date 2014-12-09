91ab74da753519e845ebe8628c91a59b32b99e12
Steve Traylen <steve.traylen@cern.ch>
2014-12-09 09:50:52 +0100

    Allow paranoia mode to be configured

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,6 +15,12 @@
 # Set to yes or no, should hosts be cached or not by nscd.
 # Default: no
 #
+# [*nscd_paranoia*]
+# Set to yes or no, should paranoia be used, default no.
+#
+# [*nscd_restart_interval*]
+# Inteval in seconds to restart at if paranoid. Default 360 seconds.
+
 # === Authors
 # Steve Traylen, CERN, <steve.traylen@cern.ch>
 #
@@ -23,6 +29,8 @@ class nscd::config ($nscd_hosts_cache  = hiera('nscd_hosts_cache','yes'),
                     $nscd_group_cache  = hiera('nscd_group_cache','no'),
                     $nscd_service_cache  = hiera('nscd_service_cache','yes'),
                     $nscd_threads = hiera('nscd_threads','4'),
+                    $nscd_paranoia  = hiera('nscd_paranoia','no'),
+                    $nscd_restart_interval  = hiera('nscd_restart_interval','3600')
                    ) {
 
 
@@ -37,6 +45,9 @@ class nscd::config ($nscd_hosts_cache  = hiera('nscd_hosts_cache','yes'),
      }
      if ! ( $nscd_service_cache in ['yes','no'] ) {
         fail("nscd_service_cache must be yes or no")
+     }
+     if ! ( $nscd_paranoia in ['yes','no'] ) {
+        fail("nscd_paranoia must be yes or no")
      }
 
 
