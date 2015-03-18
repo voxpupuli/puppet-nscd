4b36562aa23ec5ea9246c4f37cdc60bde2aeb4ad
Steve Traylen <steve.traylen@cern.ch>
2015-03-18 09:05:22 +0100

    lint fixes

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,9 @@
 # == Class: nscd::install
 # Installs nscd packages.
 #
-class nscd::install inherits nscd {
+class nscd::install (
+  $pkg_ensure = $nscd::pkg_ensure
+) inherits nscd {
   package{'nscd':
     ensure => $pkg_ensure,
   }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,9 @@
 # == Class: nscd::service
 # Controls the nscd service.
-class nscd::service inherits nscd {
+class nscd::service (
+  $service_ensure = $nscd::service_ensure,
+  $service_enable = $nscd::service_enable,
+) inherits nscd {
   service{'nscd':
     ensure     => $service_ensure,
     enable     => $service_enable,
