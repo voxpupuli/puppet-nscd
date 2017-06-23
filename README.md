3d56e9154763971eb74c63cd3c56e4cfaf57af64
Steve Traylen <steve.traylen@cern.ch>
2017-06-23 17:17:08 +0200

    Refacter to puppet 4
    This closes #16, closes #14, closes #12
    
    Parameter names now have `nscd_` prefix removed.
    
    * `nscd_hosts_cache` becomes `hosts_cache`
    * `nscd_passwd_cache` becomes  `passwd_cache`
    * `nscd_group_cache` becomes `group_cache`
    * `nscd_service_cache` becomes `services_cache`
    * `nscd_threads` becomes `threads`
    * `nscd_paranoia` becomes `paranoia`
    * `nscd_restart_interval` becomes `restart_interval`
    
    Note that it is now  `services_cache` with an `s` to be consistant
    with other parameters.
    
    The parameters `hosts_cache`, `passwd_cache`, `group_cache`,
    `services_cache` and `paranoia` are now expect booleans
    rather than `yes` or `no` string.

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-puppet-nscd
-===========
+# Puppet module for nscd
 
-Configure /etc/nscd.conf and nscd.
+[![Build Status](https://travis-ci.org/voxpupuli/puppet-nscd.png?branch=master)](https://travis-ci.org/voxpupuli/puppet-nscd)
+[![Code Coverage](https://coveralls.io/repos/github/voxpupuli/puppet-nscd/badge.svg?branch=master)](https://coveralls.io/github/voxpupuli/puppet-nscd)
+[![Puppet Forge](https://img.shields.io/puppetforge/v/puppet/nscd.svg)](https://forge.puppetlabs.com/puppet/nscd)
+[![Puppet Forge - downloads](https://img.shields.io/puppetforge/dt/puppet/nscd.svg)](https://forge.puppetlabs.com/puppet/nscd)
+[![Puppet Forge - endorsement](https://img.shields.io/puppetforge/e/puppet/nscd.svg)](https://forge.puppetlabs.com/puppet/nscd)
+[![Puppet Forge - scores](https://img.shields.io/puppetforge/f/puppet/nscd.svg)](https://forge.puppetlabs.com/puppet/nscd)
 
-Does little more than install the nscd package
-and support the on/off of databases passwd,
-groups and hosts to cache or not.
 
-See class::nscd for details.
+## Overview
 
-Tested on RHEL and Fedora.
+Configures `/etc/nscd.conf` and controls nscd service.
 
-The module assumes that hiera is available.
+## Examples
 
-[![Build Status](https://travis-ci.org/cernops/puppet-nscd.svg?branch=master)](https://travis-ci.org/cernops/puppet-nscd)
+```puppet
+include ::nscd
+```
+
+```puppet
+class{'nscd':
+  nscd_group_cache => 'yes',
+}
+
+## Usage
+
+Generated puppet strings documentation with examples is available from
+https://voxpupuli.org/puppet-nscd/
+
+## Authors
+
+* VoxPupuli <voxpupuli@groups.io>
+* Steve Traylen <steve.traylen@cern.ch>
+
+
+
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,113 +1,38 @@
-# == Class: nscd
+# nscd class.
 #
-# Manages /etc/nscd.conf file and service.
+# @summary Configures /etc/ncsd.conf and controls nscd service.
 #
-# === Parameters
-#  pkg_ensure                   Desired state of nscd package.
-#                               Default is present.
-#                               type: string
-#                               valid values: 'present' or 'absent' or 'lastest'
+# @example
+#  include ::nscd
 #
-#  service_ensure               Desired state of nscd service.
-#                               Default is running.
-#                               type: string
-#                               valid values: 'running' or 'stopped'
-#
-#  service_enable               Should nscd start on boot.
-#                               Default is true.
-#                               type: boolean
-#
-#                               valid values: true or false
-#  nscd_hosts_cache::           Should nscd cache hosts queries.
-#                               Default is yes.
-#                               type: string
-#                               valid values: 'yes' or 'no'
-#
-#  nscd_passwd_cache::          Should nscd cache passwd queries.
-#                               Default is no.
-#                               type: string
-#                               valid values: 'yes' or 'no'
-#
-#  nscd_group_cache::           Should nscd cache group queries.
-#                               Default is no.
-#                               type: string
-#                               valid values: 'yes' or 'no'
-#
-#  nscd_service_cache::         Should nscd cache service queries.
-#                               Default is yes.
-#                               type: string
-#                               valid values: 'yes' or 'no'
-#
-#  nscd_threads::               How many threads should nscd use.
-#                               Default is 4.
-#                               type: integer
-#
-#  nscd_paranoia::              Should nscd restart itself periodically.
-#                               Default is no.
-#                               type: string
-#                               valid values: 'yes' or 'no'
-#
-#  nscd_restart_interval::      The interval in that nscd restarts itself
-#                               if nscd_paranoia is enabled.
-#                               Default is 3600.
-#                               type: integer
-#
-# === Examples
-#
-#  class { 'nscd': }
-#
-# === Authors
-#
-# Steve Traylen <steve.traylen@cern.ch>
-# Arnold Bechtoldt <mail@arnoldbechtoldt.com>
-# Martin Pfeifer <martin.pfeifer@gmail.com>
-#
-# === Copyright
-#
-# Copyright 2011 CERN Steve Traylen
-#
+# @param pkg_ensure state of nscd package.
+# @param service_ensure state of nscd service ensure
+# @param service_enable state of nscd service enable
+# @param hosts_cache should nscd cache host entries
+# @param passwd_cache should nscd cache passwd entries
+# @param group_cache should nscd cache group entries
+# @param services_cache should nscd cache service entries.
+# @param threads number of threads
+# @prarm paranoia  enable internal restart mode.
+# @param restart_interval nscd internal restart interval
 class nscd (
-  $pkg_ensure            = 'present',
-  $service_ensure        = 'running',
-  $service_enable        = true,
-  $nscd_hosts_cache      = hiera('nscd_hosts_cache','yes'),
-  $nscd_passwd_cache     = hiera('nscd_passwd_cache','no'),
-  $nscd_group_cache      = hiera('nscd_group_cache','no'),
-  $nscd_service_cache    = hiera('nscd_service_cache','yes'),
-  $nscd_threads          = hiera('nscd_threads','4'),
-  $nscd_paranoia         = hiera('nscd_paranoia','no'),
-  $nscd_restart_interval = hiera('nscd_restart_interval','3600')
+  Enum['present','absent','latest'] $pkg_ensure  = 'present',
+  Boolean  $service_ensure   = true,
+  Boolean  $service_enable   = true,
+  Boolean  $hosts_cache      = true,
+  Boolean  $passwd_cache     = false,
+  Boolean  $group_cache      = false,
+  Boolean  $services_cache   = true,
+  Integer  $threads          = 4,
+  Boolean  $paranoia         = true,
+  Integer  $restart_interval = 3600,
 ) {
 
-  if ! ( $pkg_ensure in ['present','absent','latest'] ) {
-    fail('pkg_ensure must be present, absent or latest')
-  }
-  if ! ( $service_ensure in ['running','stopped'] ) {
-    fail('service_ensure must be running or stopped')
-  }
-  if ! ( $nscd_hosts_cache in ['yes','no'] ) {
-    fail('nscd_hosts_cache must be yes or no')
-  }
-  if ! ( $nscd_passwd_cache in ['yes','no'] ) {
-    fail('nscd_hosts_cache must be yes or no')
-  }
-  if ! ( $nscd_group_cache in ['yes','no'] ) {
-    fail('nscd_hosts_cache must be yes or no')
-  }
-  if ! ( $nscd_service_cache in ['yes','no'] ) {
-    fail('nscd_service_cache must be yes or no')
-  }
-  if ! ( $nscd_paranoia in ['yes','no'] ) {
-    fail('nscd_paranoia must be yes or no')
-  }
+  contain ::nscd::install
+  contain ::nscd::config
+  contain ::nscd::service
 
-  include ::nscd::install
-  include ::nscd::config
-  include ::nscd::service
-
-  anchor{"${name}::begin":}
-  -> Class['nscd::install']
+  Class['nscd::install']
   -> Class['nscd::config']
   ~> Class['nscd::service']
-  -> anchor{"${name}::end":}
 }
