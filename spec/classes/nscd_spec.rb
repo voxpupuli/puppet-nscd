6df7ba21811db5ca8d983fd457acda01b3d257e4
Steve Traylen <steve.traylen@cern.ch>
2017-06-23 15:05:26 +0200

    rubocop fixes

--- spec/classes/nscd_spec.rb
+++ spec/classes/nscd_spec.rb
@@ -1,27 +1,28 @@
 require 'spec_helper'
 describe 'nscd' do
-
   context 'with defaults for all parameters' do
-    it { should contain_class('nscd::install') } 
-    it { should contain_class('nscd::config') }
-    it { should contain_class('nscd::service') }
-    it { should contain_package('nscd').with_ensure('present')}
-    it { should contain_service('nscd').with_ensure('running')}
+    it { is_expected.to contain_class('nscd::install') }
+    it { is_expected.to contain_class('nscd::config') }
+    it { is_expected.to contain_class('nscd::service') }
+    it { is_expected.to contain_package('nscd').with_ensure('present') }
+    it { is_expected.to contain_service('nscd').with_ensure('running') }
   end
 
   context 'with pkg_ensure => "absent"' do
-    let(:params) { {:pkg_ensure => 'absent'} }
-    it { should contain_package('nscd').with_ensure('absent')}
+    let(:params) { { pkg_ensure: 'absent' } }
+
+    it { is_expected.to contain_package('nscd').with_ensure('absent') }
   end
 
   context 'with service_ensure => "stopped"' do
-    let(:params) { {:service_ensure => 'stopped'} }
-    it { should contain_service('nscd').with_ensure('stopped') }
+    let(:params) { { service_ensure: 'stopped' } }
+
+    it { is_expected.to contain_service('nscd').with_ensure('stopped') }
   end
 
   context 'with service_enable => false' do
-    let(:params) { {:service_enable => false} }
-    it { should contain_service('nscd').with_enable(false) }
-  end
+    let(:params) { { service_enable: false } }
 
+    it { is_expected.to contain_service('nscd').with_enable(false) }
+  end
 end
