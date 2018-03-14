6efb73087df9dd660a12d847f3dc8b66bd21be7c
Steve Traylen <steve.traylen@cern.ch>
2018-03-14 14:28:32 +0100

    Fixes #26 allow ttl to be configured
    Eight new variables defaulting to redhat defaults.
    
    * passwd_negative_ttl
    * passwd_positive_ttl
    * group_negative_ttl
    * group_positive_ttl
    * hosts_negative_ttl
    * hosts_positive_ttl
    * services_negative_ttl
    * services_positive_ttl

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,17 +15,33 @@
 # @param threads number of threads
 # @prarm paranoia  enable internal restart mode.
 # @param restart_interval nscd internal restart interval
+# @param passwd_positive_ttl positive time to live for passwords database.
+# @param passwd_negative_ttl negative time to live for passwords database.
+# @param group_positive_ttl positive time to live for groups database.
+# @param group_negative_ttl negative time to live for groups database.
+# @param hosts_positive_ttl positive time to live for hosts database.
+# @param hosts_negative_ttl negative time to live for hosts database.
+# @param services_positive_ttl positive time to live for services database.
+# @param services_negative_ttl negative time to live for services database.
 class nscd (
-  Enum['present','absent','latest'] $pkg_ensure       = 'present',
-  Boolean                           $service_ensure   = true,
-  Boolean                           $service_enable   = true,
-  Boolean                           $hosts_cache      = true,
-  Boolean                           $passwd_cache     = false,
-  Boolean                           $group_cache      = false,
-  Boolean                           $services_cache   = true,
-  Integer                           $threads          = 4,
-  Boolean                           $paranoia         = true,
-  Integer                           $restart_interval = 3600,
+  Enum['present','absent','latest'] $pkg_ensure            = 'present',
+  Boolean                           $service_ensure        = true,
+  Boolean                           $service_enable        = true,
+  Boolean                           $hosts_cache           = true,
+  Boolean                           $passwd_cache          = false,
+  Boolean                           $group_cache           = false,
+  Boolean                           $services_cache        = true,
+  Integer                           $threads               = 4,
+  Boolean                           $paranoia              = true,
+  Integer                           $restart_interval      = 3600,
+  Integer                           $passwd_negative_ttl   = 20,
+  Integer                           $passwd_positive_ttl   = 600,
+  Integer                           $group_negative_ttl    = 60,
+  Integer                           $group_positive_ttl    = 3600,
+  Integer                           $hosts_negative_ttl    = 20,
+  Integer                           $hosts_positive_ttl    = 3600,
+  Integer                           $services_negative_ttl = 20,
+  Integer                           $services_positive_ttl = 28800,
 ) {
 
   contain ::nscd::install
